653dba2ded9585ac8dbd5fdc1293007a80d014c0
Brian Lee <ilovenocchi@gmail.com>
2022-04-07 19:34:44 +0900

    fix: english postalCode api

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <input :value="enteredPostalCode" @change="updatePostalCode" type="text" placeholder="Please enter your 7 digit postal code">
+    <input :value="prefCode" @change="updatePrefCode" type="text" placeholder="E.g. 000"> -
+    <input :value="cityCode" @change="updateCityCode" type="text" placeholder="E.g. 0000">
     <button id="postalCodeSubmit" v-on:click="getTownName">SUBMIT</button>
     <h1>Your town is: {{currentTown}}</h1>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
@@ -14,25 +15,36 @@ import HelloWorld from './components/HelloWorld.vue'
 export default {
   name: 'App',
   data: () => ({
-    base_url: 'https://postal-codes-jp.azurewebsites.net/api/PostalCodes/',
+    base_url: 'https://madefor.github.io/postal-code-api/api/v1',
     currentTown: "",
     postalCode: "",
-    enteredPostalCode: 0,
+    enteredPostalCode: "",
+    prefCode: "",
+    cityCode: "",
   }),
    methods: {
-    updatePostalCode(event){
-      this.enteredPostalCode = event.target.value.trim()
-      this.postalCode = this.enteredPostalCode
+    updatePrefCode(event){
+      this.prefCode = event.target.value.trim()
+      this.postalCode = this.prefCode + "-" + this.cityCode
+    },
+    updateCityCode(event){
+      this.cityCode = event.target.value.trim()
+      this.postalCode = this.prefCode + "-" + this.cityCode
     },
     getTownName: async function() {
-      const response = await fetch(`${this.base_url}/${this.postalCode}`,{
+      console.log("the PS is:" + this.postalCode)
+      console.log("the PC is:" + this.prefCode)
+      console.log("the CC is:" + this.cityCode)
+      const response = await fetch(`${this.base_url}/${this.prefCode}/${this.cityCode}.json`,{
         method: 'GET',
         headers: {
           'accept': 'application/json' ,
         },
       })
       const json = await response.json()
-      this.currentTown = json[0].city.pref.name + json[0].city.name
+      console.log(json, "res")
+      this.currentTown = json.data[0].en.prefecture + ", " + json.data[0].en.address1 + ", "+ json.data[0].en.address2
+      
     },
 },
   components: {
